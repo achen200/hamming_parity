7ed88f2adb1ae7c4fee90f439c4d86d8333e8d15
achen200 <54335096+achen200@users.noreply.github.com>
2023-02-20 11:51:33 -0800

    Fixed syntax issues with bit assignments

--- src/sv files/definitions.sv
+++ src/sv files/definitions.sv
@@ -1,14 +1,14 @@
 package definitions;
 //instruction map
-    const logic [3:0]kADD   = 0b'0000;
-    const logic [3:0]kSUB   = 0b'0001;
-    const logic [3:0]kBXOR  = 0b'0010;
-    const logic [3:0]kRXOR  = 0b'0011;
-    const logic [3:0]kAND   = 0b'0100;
-    const logic [3:0]kSHIFT = 0b'0101;
-    const logic [3:0]kNOT   = 0b'0110;
-    const logic [3:0]kMOV   = 0b'0111;
-    const logic [3:0]kPASS  = 0b'1000;
+    const logic [3:0]kADD   = 'b0000;
+    const logic [3:0]kSUB   = 'b0001;
+    const logic [3:0]kBXOR  = 'b0010;
+    const logic [3:0]kRXOR  = 'b0011;
+    const logic [3:0]kAND   = 'b0100;
+    const logic [3:0]kSHIFT = 'b0101;
+    const logic [3:0]kNOT   = 'b0110;
+    const logic [3:0]kMOV   = 'b0111;
+    const logic [3:0]kPASS  = 'b1000;
 
     typedef enum logic[3:0] {
         ADD, SUB, BXOR, RXOR, AND, SHIFT, NOT, MOV, PASS
